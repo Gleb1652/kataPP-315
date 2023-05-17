840838887787c03b05ead084f9e4b6b61adc6c84
Gleb1652 <113682268+Gleb1652@users.noreply.github.com>
2023-05-17 23:20:12 +0300

    Add files via upload

---
+++ PP_3_1_5_REST_API1/src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -0,0 +1,91 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleServiceImp;
+import ru.kata.spring.boot_security.demo.service.UserServiceImp;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private final UserServiceImp userServiceImp;
+    private final RoleServiceImp roleServiceImp;
+
+    @Autowired
+    public AdminController(UserServiceImp userServiceImp, RoleServiceImp roleServiceImp) {
+        this.userServiceImp = userServiceImp;
+        this.roleServiceImp = roleServiceImp;
+    }
+
+    @GetMapping
+    public String showAllUsers(Model model, Principal principal) {
+        model.addAttribute("allUsers", userServiceImp.findAll());
+        User princ = userServiceImp.findByName(principal.getName());
+        model.addAttribute("princ", princ);
+        model.addAttribute("newUser", new User());
+        model.addAttribute("allRoles", roleServiceImp.getRoles());
+        model.addAttribute("titleTable", "Список всех пользователей:");
+        return "admin";
+    }
+
+    @GetMapping("/{id}")
+    public String showUser(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("user", userServiceImp.findOne(id));
+        model.addAttribute("titleTable", "Страница пользователя:");
+        return "user";
+    }
+
+    @GetMapping("/addUser")
+    public String addNewUser(Model model, @ModelAttribute("user") User user) {
+        List<Role> roles = roleServiceImp.getRoles();
+        model.addAttribute("rolesAdd", roles);
+        return "creation";
+    }
+
+    @PostMapping("/user-creation")
+    public String addCreateNewUser(User user) {
+        try {
+            userServiceImp.save(user);
+        } catch (Exception er) {
+            System.err.println("Пользователь с таким email уже существует!");
+        }
+        return "redirect:/admin";
+    }
+
+    @PatchMapping("/user-update")
+    public String updateUser(User user) {
+        try {
+            userServiceImp.update(user);
+        } catch (Exception e) {
+            System.err.println("Пользователь с таким логином уже существует!");
+        }
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/user-update/{id}")
+    public String updateUserForm(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("user", userServiceImp.getUser(id));
+        return "admin";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUserForm(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("user", userServiceImp.getUser(id));
+        return "admin";
+    }
+
+    @DeleteMapping("/user-delete")
+    public String deleteUser(Long id) {
+        userServiceImp.delete(id);
+        return "redirect:/admin";
+    }
+}
